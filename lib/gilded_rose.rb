089fd902bd0af4981a706938534569fbc5931388
Aga <a.dufrat@gmail.com>
2018-11-08 09:57:02 +0000

    Update previous methods and added a method for Backstage passes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,70 +6,45 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      @quality = item.quality
-      @sell_in = item.sell_in
-      generic_item if item.name == "item" || "foo"
-      aged_brie if item.name == "Aged Brie"
-      # if item.name == "Aged Brie"
-      #   return aged_brie
-      # end
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      case item.name
+        when "Aged Brie"
+          return aged_brie(item)
+        when "Sulfuras, Hand of Ragnaros"
+          return sulfuras
+        when "Backstage passes to a TAFKAL80ETC concert"
+          return backstage_pass(item)
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+      generic_item(item) if item.name != "Aged Brie" || "Sulfuras, Hand of Ragnaros" || "Backstage passes to a TAFKAL80ETC concert"
       end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
   end
+
+  def generic_item(item)
+    item.sell_in -=1
+    return item.quality if item.quality == 0 # never lowers quality below 0
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 0 # lower again after sell in date
 end
 
-def generic_item
-  @sell_in -=1
-  return if @quality == 0 # never lowers quality below 0
-  @quality -= 1
-  @quality -= 1 if @sell_in <= 0 # lower again after sell in date
-end
+  def aged_brie(item)
+    return if item.quality >= 50
+    item.quality += 1
+    return if item.quality >= 50
+    item.quality += 1 if item.sell_in <= 0
+    item.sell_in -= 1
+  end
 
-def aged_brie
-  @sell_in -= 1
-  return @quality if @quality >= 50
-  @quality += 1
-  @quality += 1 if @sell_in <= 0
+  def sulfuras
+  end
+
+  def backstage_pass(item)
+    return item.quality = 0 if item.sell_in == 0
+    return if item.quality >= 50
+    item.quality += 1 if item.sell_in
+    return if item.quality >= 50
+    item.quality += 1 if item.sell_in.between?(1, 10)
+    return if item.quality >= 50
+    item.quality += 1 if item.sell_in.between?(1, 6)
+    item.sell_in -= 1
+  end
+
 end
